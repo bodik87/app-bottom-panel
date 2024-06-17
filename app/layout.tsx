0f7873939100888ba7917b601d28fa771fe77144
Bogdan <bsshul@gmail.com>
2024-06-17 14:08:56 +0200

    Fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} flex flex-col min-h-screen`}>
+      <body className={`${inter.className} flex flex-col min-h-dvh`}>
         {children}
       </body>
     </html>
