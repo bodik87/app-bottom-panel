543212561224954867cc2a6ba16c460c0b1c857d
Bogdan <bsshul@gmail.com>
2024-06-17 13:49:22 +0200

    Fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState } from "react";
 import BottomPanel from "@/components/bottom-panel";
 import { DrawerAlert } from "@/components/drawers/drawer-alert";
 import { DrawerSingle } from "@/components/drawers/drawer-single";
@@ -5,11 +8,18 @@ import { DrawerSingle } from "@/components/drawers/drawer-single";
 type Props = {};
 
 export default function Homepage({}: Props) {
+  const [text, setText] = useState("");
   return (
     <>
       <DrawerAlert />
       <BottomPanel>
-        <div className="ml-auto">
+        <div className="w-full flex justify-between items-center">
+          <input
+            type="search"
+            className="w-full h-16 rounded-2xl px-4 text-2xl border"
+            placeholder="Text"
+          />
+
           <DrawerSingle />
         </div>
       </BottomPanel>
--- components/drawers/drawer-single.tsx
+++ components/drawers/drawer-single.tsx
@@ -9,15 +9,15 @@ export function DrawerSingle() {
   return (
     <Drawer.Root shouldScaleBackground>
       <Drawer.Trigger asChild>
-        <button className="flex justify-center w-20">
+        <button className="flex justify-center items-center w-20 h-20">
           <AlignJustify size={30} />
         </button>
       </Drawer.Trigger>
       <Drawer.Portal>
         <Drawer.Overlay className="fixed inset-0 bg-black/40" />
-        <Drawer.Content className="max-w-md mx-auto bg-white flex flex-col rounded-t-2xl max-h-[96%] fixed bottom-0 left-0 right-0 flex-1 pt-4">
+        <Drawer.Content className="max-w-md mx-auto bg-white flex flex-col rounded-t-2xl max-h-[96%] fixed bottom-0 left-0 right-0 flex-1 pt-4 outline-none">
           <DragButton />
-          <div className="mt-4 px-4 w-full">
+          <div className="mt-4 px-4 w-full text-lg">
             <h3 className="font-medium">Title</h3>
             <p className="text-gray-600 my-4">Content</p>
           </div>
