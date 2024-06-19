48fbb76cea16045ed56c4595feaca7e9f2d0105a
Bogdan <bsshul@gmail.com>
2024-06-19 12:19:49 +0200

    Search icon

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,8 @@ import { useState } from "react";
 import BottomPanel from "@/components/bottom-panel";
 import { DrawerSingle } from "@/components/drawers/drawer-single";
 import Carousel from "@/components/carousel";
-import { Search } from "@/components/drawers/search";
+import { Search as SearchPanel } from "@/components/drawers/search";
+import { Search } from "lucide-react";
 
 type Props = {};
 
@@ -15,15 +16,16 @@ export default function Homepage({}: Props) {
     <>
       <Carousel />
 
-      <Search open={open} setOpen={setOpen} />
+      <SearchPanel open={open} setOpen={setOpen} />
 
       <BottomPanel>
         <div className="w-full pl-4 flex justify-between items-center">
           <div
             onClick={() => setOpen(true)}
-            className="w-full bg-white flex items-center text-gray-500 h-[50px] rounded-xl px-4 text-xl cursor-text hover:ring-1 hover:ring-gray-300 transition-all"
+            className="w-full bg-white flex items-center justify-between text-gray-500 h-[50px] rounded-xl px-4 text-xl cursor-text hover:ring-1 hover:ring-gray-300 transition-all"
           >
-            Search...
+            <span> Search...</span>
+            <Search />
           </div>
 
           <DrawerSingle />
